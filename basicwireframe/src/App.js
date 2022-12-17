ebb0a92541e32e589307d7d32ce97880b2bd1fe1
intelora-maduri <113329000+intelora-maduri@users.noreply.github.com>
2022-12-17 16:10:11 +0530

    SpeechBubble code
    SpeechBubble code

--- basicwireframe/src/App.js
+++ basicwireframe/src/App.js
@@ -1,12 +1,14 @@
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Container from './components/container/Conatiner';
+import SpeechBubble from './components/SpeechBubble/SpeechBubble';
 
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Header/>
+      <SpeechBubble/> 
       <Container/>
       <Footer/>
     </div>
---
+++ basicwireframe/src/components/SpeechBubble/SpeechBubble.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './SpeechBubble.css';
+
+const SpeechBubble=()=> {
+    return (
+  <div className="cartoon">
+  <div className="shirt-neck shirt-neck-back b"></div>
+  <div className="hair hair-back-bottom r b"></div>
+  <div className="hair hair-back hair-back-back r b"></div>
+  <div className="shirt b r"></div>
+  <div className="shirt-line-2 b"></div>
+  <div className="head r"></div>
+  <div className="hair hair-back r b"></div>
+  <div className="ear r ha hb"></div>
+  <div className="nose r"></div>
+  <div className="hair-container">
+  <div className="hair hair-top r b ha"></div></div>
+  <div className="eye r b"></div>
+  <div className="mouth r ha"></div>
+  <div className="shirt-line b"></div>
+  <div className="shirt-neck b"></div>
+  <div className="eyebrow r"></div>
+  <div className="eyebrow r"></div>
+  <div className="bubble b r hb">Hello<br/>Intelora...</div>
+  </div>
+    );
+  }
+  export default SpeechBubble;
